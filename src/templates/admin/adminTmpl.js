8718334e287165682fe866b901dd73e5b2519c2b
Oleksandr Ryzhkov <ryzhman@gmail.com>
2017-01-22 22:03:55 +0200

    Styles were enhanced

--- src/templates/admin/adminTmpl.js
+++ src/templates/admin/adminTmpl.js
@@ -7,63 +7,63 @@ import html from "html-template-tag";
 //creates a template for admin greeting
 const adminWelcomeInfo = loginDt => html`
     <h2>
-    Hello ${loginDt.name}! ${loginDt.lastVisit ? `Last time you visited this page ${loginDt.lastVisit}`: ``}
+    Hello ${loginDt.name}! ${loginDt.lastVisit ? `Last time you visited this page ${loginDt.lastVisit}` : ``}
     </h2>
 `;
 
 //creates a template of table with all users
 const usersListTmpl = uList => html`
-        <table>
-        <caption>The list of all users</caption>
-        <th>Name</th>
-        <th>Password</th>
-        <th>Access</th>
-        <th>Last visit</th>
-        <th>Group</th>
-        <th>Remove</th>
-        ${uList.map(usr => html`
-            <tr>
-            <td>${usr.name}</td>
-            <td>${usr.pass}</td>
-            <td>${usr.access}</td>
-            <td>${usr.lastVisit}</td>
-            <td>${usr.group}</td>
-            <td><button class="removeUser" id="${usr.id}">✗</button></td>
-            </tr>
-        `)}
+        <table id="hor-zebra">
+            <caption>The list of all users</caption>
+            <th>Name</th>
+            <th>Password</th>
+            <th>Access</th>
+            <th>Last visit</th>
+            <th>Group</th>
+            <th>Remove</th>
+            ${uList.map(usr => html`
+                <tr ${usr.id % 2 ? `` : `class="odd"`}>
+                    <td>${usr.name}</td>
+                    <td>${usr.pass}</td>
+                    <td>${usr.access}</td>
+                    <td>${usr.lastVisit ? `${usr.lastVisit}` : `n/a`}</td>
+                    <td>${usr.group}</td>
+                    <td><button class="removeUser" id="${usr.id}">✗</button></td>
+                </tr>
+            `)}
         </table>
     `;
 
 //creates a template of table with all questions
 const quizesListTmpl = qList => html`
-        <table>
-        <caption>The list of all questions</caption>
-        <th>Question text</th>
-        <th>Options</th>
-        <th>Correct answers</th>
-        <th>Type of a question</th>
-        <th>Remove</th>
-        ${qList.map(quiz => html`
-            <tr>
-            <td>${quiz.text}</td>
-            <td>
-                <ul>
-                    ${quiz.options.map(option => html`
-                    <li> ${option} </li>
-                    `)}
-                </ul>
-            </td>
-            <td>
-                <ul>
-                    ${quiz.answer.map(answr => html`
-                    <li>${answr}</li>
-                    `)}
-                </ul>
-            </td>
-            <td>${quiz.type}</td>
-            <td><button class="removeQuiz" id="${quiz.id}">✗</button></td>
-            </tr>
-        `)}
+        <table id="hor-zebra">
+            <caption>The list of all questions</caption>
+            <th>Question text</th>
+            <th>Options</th>
+            <th>Correct answers</th>
+            <th>Type of a question</th>
+            <th>Remove</th>
+            ${qList.map(quiz => html`
+                <tr ${quiz.id % 2 ? `` : `class="odd"`}>
+                <td>${quiz.text}</td>
+                <td>
+                    <ul>
+                        ${quiz.options.length === 0 ? `n/a` : quiz.options.map(option => `
+                            <li> ${option} </li>
+                        `)}
+                    </ul>
+                </td>
+                <td>
+                    <ul>
+                        ${quiz.answer.map(answr => html`
+                        <li>${answr}</li>
+                        `)}
+                    </ul>
+                </td>
+                <td>${quiz.type}</td>
+                <td><button class="removeQuiz" id="${quiz.id}">✗</button></td>
+                </tr>
+            `)}
         </table>
     `;
 
